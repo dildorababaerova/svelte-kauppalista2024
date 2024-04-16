0e35b5a3476c26927e1a4de0d57a162615fe4bb6
Dildora <48212@edu.raseko.fi>
2024-04-16 10:38:17 +0300

    lisätty

--- kauppalista/src/routes/+page.svelte
+++ kauppalista/src/routes/+page.svelte
@@ -1,4 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+    let jutut  = ["Leipä", "Tee", "Voi"]
+
+let uusiAsia = '';
+
+function lisääAsia() {
+    jutut.push(uusiAsia);
+    jutut = jutut;
+    uusiAsia = '';
+}
+</script>
+
+<div class= "komponentti">
+<h1>Kauppalista</h1>
+<ul>
+    {#each jutut as juttu}
+        <li>{juttu}</li>
+    {/each}
+</ul>
+<form class="uusi">
+    <label for="uusi-asia">Lisää uusi asia</label>
+    <br>
+    <input id="uusi-asia" type="text" bind:value={uusiAsia} />
+    <button on:click|preventDefault={lisääAsia}>Lisää</button>
+</form>
+
+</div>
+<style>
+    h1 {
+        background-color: hwb(221 48% 26% / 0.911);
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+    ul {
+        color: blueviolet;
+        font-size: 200%;
+        background-color: bisque;
+    }
+
+</style>
+
+ 
+
+    
